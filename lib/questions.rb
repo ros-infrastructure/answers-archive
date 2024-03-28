4db4fb88bcfba6182df1fab131dbae372560c04d
Steven! Ragnarök <steven@nuclearsandwich.com>
2024-03-28 13:16:02 -0700

    Update Questions DataSource to use both ROS and Gazebo data.

--- lib/questions.rb
+++ lib/questions.rb
@@ -3,31 +3,98 @@ require "json"
 class QuestionsDataSource < Nanoc::DataSource
   identifier :questions
 
-  def up
-    # TODO parameterize and add other site sources.
-    @gz_answers = JSON.load(File.read('answers/final_gazebo_se_dump.json'))["Questions"]
+  class Question < Struct.new(
+    :answers,
+    :body_markdown,
+    :creation_date,
+    :migrated_url,
+    :original_post_id,
+    :original_post_url,
+    :owner_display_name,
+    :site,
+    :tags,
+    :title,
+    :upvote_count,
+    keyword_init: true
+  )
+
+    def self.from_json json
+      self.new(
+        original_post_id: json['OriginalPostID'],
+        title: json['Title'],
+        body_markdown: json['BodyMarkdown'],
+        owner_display_name: json['OwnerDisplayName'],
+        tags: json["Tags"].split(","),
+        creation_date: json['CreationDate'], # TODO PARSE IT?
+        upvote_count: json['UpvoteCount'],
+        original_post_url: json['OriginalPostURL'],
+        answers: json['Answers'].map{|answer| Answer.from_json answer},
+      )
+    end
+
+    def attributes
+      @attributes ||= self.to_h
+    end
   end
 
-  def attributes_from_json json
-    Hash[
-      original_post_id: json['OriginalPostID'],
-      title: json['Title'],
-      owner_display_name: json['OwnerDisplayName'],
-      creation_date: json['CreationDate'],
-      upvote_count: json['UpvoteCount'],
-      original_post_url: json['OriginalPostURL'],
-      # TODO answers
-    ]
+  class Answer < Struct.new(
+    :accepted?,
+    :body_markdown,
+    :creation_date,
+    :original_post_id,
+    :owner_display_name,
+    :upvote_count,
+    keyword_init: true
+  )
+    def self.from_json json
+      self.new(
+        original_post_id: json["OriginalPostID"],
+        owner_display_name: json["OwnerDisplayName"],
+        creation_date: json["CreationDate"],
+        body_markdown: json["BodyMarkdown"],
+        accepted?: json["Accepted"],
+        upvote_count: json["UpvoteCount"],
+      )
+    end
+  end
+
+  def up
+    @ros_questions = JSON.load(File.read('answers/ros_unexported_se_dump.json'))["Questions"] +
+      JSON.load(File.read('answers/ros_se_dump.json'))["Questions"]
+
+    @gz_questions = JSON.load(File.read('answers/gazebosim_unexported_se_dump.json'))["Questions"] +
+      JSON.load(File.read('answers/gazebo_se_dump.json'))["Questions"]
+
+
+    @ros_migration_map = Hash.new
+    JSON.load(File.read('answers/ros-to-robotics.json'))["Posts"].each do |post|
+      @ros_migration_map[post["OriginalPostID"]] = post["NewUrl"]
+    end
+
+    @gz_migration_map = Hash.new
+    JSON.load(File.read('answers/gazebo-to-robotics.json'))["Posts"].each do |post|
+      @gz_migration_map[post["OriginalPostID"]] = post["NewUrl"]
+    end
   end
 
   def items
-    @gz_answers.map do |question|
-      attributes = attributes_from_json(question)
-      attributes[:site] = 'Gazebo Answers'
+    @gz_questions.map do |question|
+      q = Question.from_json question
+      q.site = 'Gazebo Answers'
+      q.migrated_url = @gz_migration_map[q.original_post_id]
       new_item(
-        question['BodyMarkdown'],
-        attributes,
-        Nanoc::Identifier.new("/gz/question/#{question["OriginalPostID"]}.md")
+        q.body_markdown,
+        q.attributes,
+        Nanoc::Identifier.new("/gz/question/#{q.original_post_id}.md")
+      )
+    end + @ros_questions.map do |question|
+      q = Question.from_json question
+      q.site = 'ROS Answers'
+      q.migrated_url = @ros_migration_map[q.original_post_id]
+      new_item(
+        q.body_markdown,
+        q.attributes,
+        Nanoc::Identifier.new("/ros/question/#{q.original_post_id}.md")
       )
     end
   end
