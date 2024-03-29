8d22bc5370ac10266833b9de3e213332a1861dee
Steven! Ragnarök <steven@nuclearsandwich.com>
2024-03-28 17:46:52 -0700

    Refactor so questions and answers are both created as items.
    I moved the new classes into the top level and create Nanoc::Items for
    both questions and answers.

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,101 +1,137 @@
 require "json"
 
+class Question < Struct.new(
+  :answers,
+  :body_markdown,
+  :creation_date,
+  :original_post_id,
+  :original_post_url,
+  :owner_display_name,
+  :site,
+  :tags,
+  :title,
+  :upvote_count,
+  keyword_init: true
+)
+
+  def self.from_json json
+    self.new(
+      original_post_id: json['OriginalPostID'],
+      title: json['Title'],
+      body_markdown: json['BodyMarkdown'],
+      owner_display_name: json['OwnerDisplayName'],
+      tags: json["Tags"].split(","),
+      creation_date: json['CreationDate'], # TODO PARSE IT?
+      upvote_count: json['UpvoteCount'],
+      original_post_url: json['OriginalPostURL'],
+      answers: json['Answers'].map{|answer| Answer.from_json answer},
+    )
+  end
+
+  def attributes
+    @attributes ||= self.to_h
+  end
+
+  def migrated_url
+    self.site.migration_map[self.original_post_id]
+  end
+
+  def nanoc_identifier
+    @nanoc_identifier ||= Nanoc::Identifier.new(
+      "/#{self.site.id}/question/#{self.original_post_id}.md"
+    )
+  end
+end
+
+class Answer < Struct.new(
+  :accepted?,
+  :body_markdown,
+  :creation_date,
+  :original_post_id,
+  :owner_display_name,
+  :upvote_count,
+  keyword_init: true
+)
+  def self.from_json json
+    self.new(
+      original_post_id: json["OriginalPostID"],
+      owner_display_name: json["OwnerDisplayName"],
+      creation_date: json["CreationDate"],
+      body_markdown: json["BodyMarkdown"],
+      accepted?: json["Accepted"],
+      upvote_count: json["UpvoteCount"],
+    )
+  end
+
+  def attributes
+    self.to_h
+  end
+
+  def nanoc_identifier
+    @nanoc_identifier ||= Nanoc::Identifier.new(
+      "/answer/#{self.original_post_id}.md"
+    )
+  end
+end
+
+class Site < Struct.new(:id, :title, :migration_map, keyword_init: true)
+end
+
 class QuestionsDataSource < Nanoc::DataSource
   identifier :questions
 
-  class Question < Struct.new(
-    :answers,
-    :body_markdown,
-    :creation_date,
-    :migrated_url,
-    :original_post_id,
-    :original_post_url,
-    :owner_display_name,
-    :site,
-    :tags,
-    :title,
-    :upvote_count,
-    keyword_init: true
-  )
-
-    def self.from_json json
-      self.new(
-        original_post_id: json['OriginalPostID'],
-        title: json['Title'],
-        body_markdown: json['BodyMarkdown'],
-        owner_display_name: json['OwnerDisplayName'],
-        tags: json["Tags"].split(","),
-        creation_date: json['CreationDate'], # TODO PARSE IT?
-        upvote_count: json['UpvoteCount'],
-        original_post_url: json['OriginalPostURL'],
-        answers: json['Answers'].map{|answer| Answer.from_json answer},
-      )
-    end
-
-    def attributes
-      @attributes ||= self.to_h
-    end
-  end
-
-  class Answer < Struct.new(
-    :accepted?,
-    :body_markdown,
-    :creation_date,
-    :original_post_id,
-    :owner_display_name,
-    :upvote_count,
-    keyword_init: true
-  )
-    def self.from_json json
-      self.new(
-        original_post_id: json["OriginalPostID"],
-        owner_display_name: json["OwnerDisplayName"],
-        creation_date: json["CreationDate"],
-        body_markdown: json["BodyMarkdown"],
-        accepted?: json["Accepted"],
-        upvote_count: json["UpvoteCount"],
-      )
-    end
-  end
-
   def up
-    @ros_questions = JSON.load(File.read('answers/ros_unexported_se_dump.json'))["Questions"] +
-      JSON.load(File.read('answers/ros_se_dump.json'))["Questions"]
-
-    @gz_questions = JSON.load(File.read('answers/gazebosim_unexported_se_dump.json'))["Questions"] +
-      JSON.load(File.read('answers/gazebo_se_dump.json'))["Questions"]
-
-
-    @ros_migration_map = Hash.new
+    ros_migration_map = Hash.new
     JSON.load(File.read('answers/ros-to-robotics.json'))["Posts"].each do |post|
-      @ros_migration_map[post["OriginalPostID"]] = post["NewUrl"]
+      ros_migration_map[post["OriginalPostID"]] = post["NewUrl"]
+    end
+    ros_site = Site.new(id: "ros", title: "ROS Answers", migration_map: ros_migration_map)
+
+    gz_migration_map = Hash.new
+    JSON.load(File.read('answers/gazebo-to-robotics.json'))["Posts"].each do |post|
+      gz_migration_map[post["OriginalPostID"]] = post["NewUrl"]
+    end
+    gz_site = Site.new(id: "gz", title: "Gazebo Answers", migration_map: gz_migration_map)
+
+    @questions = Array.new
+
+    (JSON.load(File.read('answers/ros_unexported_se_dump.json'))["Questions"] +
+     JSON.load(File.read('answers/ros_se_dump.json'))["Questions"]).select{|json|
+       json["Answers"].size > 0}.take(10).each do |q_json|
+      question = Question.from_json q_json
+      question.site = ros_site
+      @questions << question
     end
 
-    @gz_migration_map = Hash.new
-    JSON.load(File.read('answers/gazebo-to-robotics.json'))["Posts"].each do |post|
-      @gz_migration_map[post["OriginalPostID"]] = post["NewUrl"]
+   (JSON.load(File.read('answers/gazebosim_unexported_se_dump.json'))["Questions"] +
+    JSON.load(File.read('answers/gazebo_se_dump.json'))["Questions"]).select{|json|
+      json["Answers"].size > 0}.take(10).each do |q_json|
+      question = Question.from_json q_json
+      question.site = gz_site
+      @questions << question
     end
   end
 
   def items
-    @gz_questions.map do |question|
-      q = Question.from_json question
-      q.site = 'Gazebo Answers'
-      q.migrated_url = @gz_migration_map[q.original_post_id]
-      new_item(
-        q.body_markdown,
-        q.attributes,
-        Nanoc::Identifier.new("/gz/question/#{q.original_post_id}.md")
+    return @items if defined? @items
+
+    @items = Array.new
+    @questions.each do |question|
+      @items << new_item(
+        question.body_markdown,
+        question.attributes,
+        question.nanoc_identifier
       )
-    end + @ros_questions.map do |question|
-      q = Question.from_json question
-      q.site = 'ROS Answers'
-      q.migrated_url = @ros_migration_map[q.original_post_id]
-      new_item(
-        q.body_markdown,
-        q.attributes,
-        Nanoc::Identifier.new("/ros/question/#{q.original_post_id}.md")
-      )
+      if question.answers
+        question.answers.each do |answer|
+          @items << new_item(
+            answer.body_markdown,
+            answer.attributes,
+            Nanoc::Identifier.new(question.nanoc_identifier.without_ext + answer.nanoc_identifier)
+          )
+        end
+      end
     end
+    @items
   end
 end
